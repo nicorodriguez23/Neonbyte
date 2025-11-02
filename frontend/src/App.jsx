ebd31f7a921d3b9bec461bfb0086d15fa8d00e53
Nicolas Rodriguez <nicorodriguez953@gmail.com>
2025-11-02 19:09:53 -0300

    actualizacion de letra

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
+import Footer from "./components/footer.jsx";
 
 
 import Home from "./pages/Home";
