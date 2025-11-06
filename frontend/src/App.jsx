92152c7fbe77effa10f89a733c3374f722c188b6
Nicolas Rodriguez <nicorodriguez953@gmail.com>
2025-11-06 10:54:20 -0300

    cambio en import footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
+import Footer from "./components/Footer";
 
 
 import Home from "./pages/Home";
